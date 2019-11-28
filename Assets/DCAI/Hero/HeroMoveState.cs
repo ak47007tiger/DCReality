6ff233d5db34417098cc97f6987fab427d04e821
ak47007tiger <ak47007tiger@163.com>
2019-11-28 08:31:45 +0800

    auto

--- Assets/DCAI/Hero/HeroMoveState.cs
+++ Assets/DCAI/Hero/HeroMoveState.cs
@@ -4,58 +4,58 @@ using UnityEngine;
 
 namespace DC.AI
 {
-  public class HeroMoveState : BaseHeroState
-  {
-    public override void Reason(object data)
+    public class HeroMoveState : BaseHeroState
     {
-      base.Reason(data);
-
-      //to skill state
-      if (CheckToSkillState())
-      {
-        return;
-      }
-      // var skillKeyList = Hero.mHeroCfg.GetSkillKeyList();
-      // foreach (var keyCode in skillKeyList)
-      // {
-      //     if (Input.GetKeyDown(keyCode))
-      //     {
-      //         return;
-      //     }
-      // }
-    }
-
-    public override void DoBeforeLeaving()
-    {
-      base.DoBeforeLeaving();
-      Actor.NavTraceTarget.SetStop(true);
-      Actor.NavArrivePosition.SetStop(true);
-    }
-
-    public override void Act(object data)
-    {
-      base.Act(data);
-
-      if (Input.GetMouseButtonDown(1))
-      {
-        var ray = DCGraphics.Instance.MainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
+        public override void Reason(object data)
         {
-          if (SystemPreset.IsGround(hit.transform.gameObject.tag))
-          {
-            //修改移动位置
-            var hitPoint = hit.point;
-            if (Actor.IsAutoMoving())
-            {
-              Actor.StopAutoMove();
-            }
+            base.Reason(data);
 
-            Actor.NavArrivePosition.StartTrace(hitPoint, SystemPreset.move_stop_distance);
-            //                        MsgSys.Send(GameEvent.ClickEnvGround, hitPoint);
-          }
+            //to skill state
+            if (CheckToSkillState())
+            {
+                return;
+            }
+            // var skillKeyList = Hero.mHeroCfg.GetSkillKeyList();
+            // foreach (var keyCode in skillKeyList)
+            // {
+            //     if (Input.GetKeyDown(keyCode))
+            //     {
+            //         return;
+            //     }
+            // }
         }
-      }
+
+        public override void DoBeforeLeaving()
+        {
+            base.DoBeforeLeaving();
+            Actor.NavTraceTarget.SetStop(true);
+            Actor.NavArrivePosition.SetStop(true);
+        }
+
+        public override void Act(object data)
+        {
+            base.Act(data);
+
+            if (Input.GetMouseButtonDown(1))
+            {
+                var ray = DCGraphics.Instance.MainCamera.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (SystemPreset.IsGround(hit.transform.gameObject.tag))
+                    {
+                        //修改移动位置
+                        var hitPoint = hit.point;
+                        if (Actor.IsAutoMoving())
+                        {
+                            Actor.StopAutoMove();
+                        }
+
+                        Actor.NavArrivePosition.StartTrace(hitPoint, SystemPreset.move_stop_distance);
+                        //                        MsgSys.Send(GameEvent.ClickEnvGround, hitPoint);
+                    }
+                }
+            }
+        }
     }
-  }
 }
